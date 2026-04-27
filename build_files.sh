76e0fda3fda55270e89fa2b15bd4b8409acf58ee
albert <albertcyriac04@gmail.com>
2026-04-27 15:54:52 +0530

    Fix Vercel build: add --break-system-packages to pip install

--- build_files.sh
+++ build_files.sh
@@ -3,7 +3,7 @@
 echo "BUILD START"
 
 # Install dependencies
-python3.12 -m pip install -r requirements.txt
+python3.12 -m pip install -r requirements.txt --break-system-packages
 
 # Run migrations (optional during build, usually better to run in post-deployment)
 # python3.12 manage.py migrate --noinput
